c6d34d8d9fdf0d4d3306e643148dce1052bc8393
gulifan <gulifan@dxy.cn>
2019-12-12 18:14:36 +0800

    feat(all the file): 处理请求header，上一次提交的post请求修复

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,20 @@
 import { AxiosRequestConfig } from './types'
 
 export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url, true)
+
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      // 当我们传入的 data 为空的时候，请求 header 配置 Content-Type 是没有意义的，于是我们把它删除。
+      delete headers[name]
+    } else {
+      request.setRequestHeader(name, headers[name])
+    }
+  })
 
   request.send(data)
 }
