d333f32dcf92f33da330bd4f4412d225cb0acefd
gulifan <gulifan@dxy.cn>
2020-01-19 17:21:49 +0800

    feat(all the file): 配置合并及策略

---
+++ src/core/mergeConfig.ts
@@ -0,0 +1,67 @@
+import { AxiosRequestConfig } from '../types'
+import { isPlainObject, deepMerge } from '../helpers/util'
+
+const strats = Object.create(null)
+
+function defaultStrat(val1: any, val2: any): any {
+  return typeof val2 !== 'undefined' ? val2 : val1
+}
+
+function fromVal2Start(val1: any, val2: any): any {
+  if (typeof val2 !== 'undefined') {
+    return val2
+  }
+}
+
+function deepMergeStrat(val1: any, val2: any): any {
+  if (isPlainObject(val2)) {
+    return deepMerge(val1, val2)
+  } else if (typeof val2 !== undefined) {
+    return val2
+  } else if (isPlainObject(val1)) {
+    return deepMerge(val1)
+  } else if (typeof val1 !== 'undefined') {
+    return val1
+  }
+}
+
+const stratKeysDeepMerge = ['headers']
+
+stratKeysDeepMerge.forEach(key => {
+  strats[key] = deepMergeStrat
+})
+
+const stratKeysFromVal2 = ['url', 'params', 'data']
+
+stratKeysFromVal2.forEach(key => {
+  strats[key] = fromVal2Start
+})
+
+export default function mergeConfig(
+  config1: AxiosRequestConfig,
+  config2?: AxiosRequestConfig
+): AxiosRequestConfig {
+  // config1 代表默认配置，config2 代表自定义配置
+  if (!config2) {
+    config2 = {}
+  }
+
+  const config = Object.create(null)
+
+  for (const key in config2) {
+    mergeField(key)
+  }
+
+  for (const key in config1) {
+    if (!config2[key]) {
+      mergeField(key)
+    }
+  }
+
+  function mergeField(key: string): void {
+    const strat = strats[key] || defaultStrat
+    config[key] = strat(config1[key], config2![key])
+  }
+
+  return config
+}
